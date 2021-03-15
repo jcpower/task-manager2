b5f0872e7b1c1e77078cb1ccf1082f5ff7c1f9f0
jcpower <lasombra@gmail.com>
2021-03-15 07:59:36 -0400

    Challenge Upload Avatar
    Postman is giving a 500 error.  MulterError: Unexpected field

--- src/index.js
+++ src/index.js
@@ -6,17 +6,16 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
-// app.use((req,res, next) => {
-//     if (req.method === 'GET') {
-//         res.send('Get requests are disabled')
-//     } else {
-//         next()
-//     }
-// })
 
-// app.use((req, res, next) => {
-//     res.status(503).send('Currently under construction, come back later')
-//     })
+const multer = require('multer')
+const upload = multer({
+    dest: 'images'
+})
+app.post('/upload', upload.single('upload'), (req, res) => {
+    res.send()
+})
+
+
 
 app.use(express.json())
 app.use(userRouter)
@@ -26,18 +25,3 @@ app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
 
-const Task = require('./models/task')
-const User = require('./models/user')
-
-const main = async () => {
-    // const task = await Task.findById('604628290a8aacb72107c9a1')
-    // await task.populate('owner').execPopulate()
-    // console.log(task.owner)
-
-    const user = await User.findById('604627470b3cdbb69d0c39bb')
-    await user.populate('tasks').execPopulate()
-    console.log(user.tasks) 
-
-}
-
-main()
--- src/routers/task.js
+++ src/routers/task.js
@@ -17,9 +17,31 @@ router.post('/tasks', auth, async (req, res) => {
     }
 })
 
-router.get('/tasks', auth,async (req, res) => {
+//GET /tasks?limit=10&skip=0
+//GET /tasks?completed=false or false
+//GET /tasks?sortBy=createdAt:asc
+router.get('/tasks', auth, async (req, res) => {
+    const match = {}
+    const sort ={}
+    if (req.query.completed) {
+        match.completed = req.query.completed === 'true'
+    }
+
+    if (req.query.sortBy) {
+        const parts = req.query.sortBy.split(':')
+        sort[parts[0]] = parts[1] === 'desc' ? -1 : 1 
+    }
+
     try {
-        await req.user.populate('tasks').execPopulate()
+        await req.user.populate({
+            path: 'tasks',
+            match,
+            options: {
+                limit: parseInt(req.query.limit),
+                skip: parseInt(req.query.skip),
+                sort
+            }
+        }).execPopulate()
         res.send(req.user.tasks)
     } catch (e) {
         res.status(500).send()
@@ -42,7 +64,7 @@ router.get('/tasks/:id',auth, async (req, res) => {
     }
 })
 
-router.patch('/tasks/:id', async (req, res) => {
+router.patch('/tasks/:id', auth, async (req, res) => {
     const updates = Object.keys(req.body)
     const allowedUpdates = ['description', 'completed']
     const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
@@ -66,18 +88,19 @@ router.patch('/tasks/:id', async (req, res) => {
     }
 })
 
-router.delete('/tasks/:id', async (req, res) => {
+router.delete('/tasks/:id',auth, async (req, res) => {
     try {
-        const task = await Task.findByIdAndDelete(req.params.id)
-
+        const task = await Task.findOneAndDelete ({ _id: req.params.id, owner: req.user._id })
         if (!task) {
              res.status(404).send()
     }
     
         res.send(task)
-    } catch (e) {
+    } catch (e) { 
         res.status(500).send()
     }
 })
 
 module.exports = router 
+
+
