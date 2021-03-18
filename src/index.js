03e3c9ac9eb244ef0055f9b0398f575410c73d4f
jcpower <lasombra@gmail.com>
2021-03-18 08:23:54 -0400

    Uploads validation
    Both pictures and documents

--- src/index.js
+++ src/index.js
@@ -9,10 +9,25 @@ const port = process.env.PORT || 3000
 
 const multer = require('multer')
 const upload = multer({
-    dest: 'images'
+    dest: 'images',
+    limits: {
+        fileSize: 1000000
+    },
+    fileFilter(req, file, cb) {
+        if (!file.originalname.match(/\.(doc|docx)$/)) {
+            return cb(new Error('Please upload a Word document'))
+        }
+
+        cb(undefined, true)
+    }
 })
+const errorMiddleware = (req, res, next) => {
+    throw new Error('From my middleware')
+}
 app.post('/upload', upload.single('upload'), (req, res) => {
     res.send()
+}, (error, req, res, next) => {
+    res.status(400).send({error: error.message})
 })
 
 
